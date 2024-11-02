0606ebcd25ebb8f95da6aad74ba1705f936a4567
yujin3iplaywright <yujin3i+playwright@gmail.com>
2024-11-02 16:53:52 +0900

    9.2のコードを追加

---
+++ chapter9/9_2/tests/List9_2_api.spec.ts
@@ -0,0 +1,18 @@
+/*
+テスト実行前にファイルを以下のように配置してください。
+my-app/
+├ src/
+│ └ index.ts こちらは中身を書き換えます。
+├ tests
+│ └ List9_2_api.spec.ts
+
+`npm run dev`でアプリを起動後にUIモードでテストを実行します
+*/
+
+import { test, expect } from '@playwright/test';
+
+test('api', async ({ request }) => {
+    const result = await request.get('http://localhost:3000/')
+    expect(result.ok()).toBeTruthy()
+    expect(await result.text()).toEqual('Hello Hono!')
+})
