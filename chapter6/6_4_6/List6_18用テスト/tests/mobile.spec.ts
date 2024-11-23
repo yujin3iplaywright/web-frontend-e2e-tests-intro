24f9ecf447f2162e21af9b399551c8b0fdd37d8a
yujin3iplaywright <yujin3i+playwright@gmail.com>
2024-11-23 22:13:05 +0900

    実行環境について追記

--- chapter6/6_4_6/List6_18用テスト/tests/mobile.spec.ts
+++ chapter6/6_4_6/List6_18用テスト/tests/mobile.spec.ts
@@ -1,3 +1,18 @@
+/*
+このテストコードの動作確認は以下の環境で行いました。
+ - CPU アーキテクチ: arm64（Apple M1）
+ - OS バージョン: macOS Sequoia 15.1.1
+ - Node.js バージョン: 22.9.0
+
+テスト実行前にファイルを以下のように配置してください。
+playwright-handson/
+├ app
+│ ├ globals.css こちらは中身を書き換えます
+│ └ page.tsx こちらは中身を書き換えます
+├ tests/
+│ └ mobile.spec.ts
+*/
+
 import { test, expect } from '@playwright/test';
 
 test('モバイルモードでボタンをタッチしてクリックできる', async ({ page }) => {
