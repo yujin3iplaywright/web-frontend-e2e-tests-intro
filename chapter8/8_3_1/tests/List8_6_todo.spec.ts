25f1a20d22711c8a750ccc181bec0d1b69e62664
Yuji Nagatomo <yujin3i+cicd@gmail.com>
2025-01-10 08:27:24 +0900

    ファイルの配置についてコメントを追加

--- chapter8/8_3_1/tests/List8_6_todo.spec.ts
+++ chapter8/8_3_1/tests/List8_6_todo.spec.ts
@@ -1,3 +1,18 @@
+/*
+テスト実行前に以下のように配置してください。
+playwright-handson/
+├ app/
+│ └ page.tsx こちらは中身を書き換えます。また globals.css は中身を空にしてください。
+├ tests/
+│ └ List8_5_todo.spec.ts
+│ └ List8_6_todo.spec.ts
+│ └ List8_7_todo.spec.ts
+│ └ List8_8_todo-page.ts
+│ └ List8_9_todo.spec.ts
+
+上記の状態で `npm run dev` でアプリを起動して
+*/
+
 import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }, testInfo) => {
